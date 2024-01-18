c639a2691d216dcd8d0fd36608931f2fb7ee8d5f
millywanderi <wanderinyambura5@gmail.com>
2024-01-18 08:23:36 +0300

    SQL script that creates a table users with country

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- In and not out
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'IN') NOT NULL,
+    PRIMARY KEY (id)
+);
