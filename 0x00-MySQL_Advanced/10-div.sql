407dd0a7166c4953fe3a1c0721023c6337745d9e
millywanderi <wanderinyambura5@gmail.com>
2024-01-18 09:21:57 +0300

    creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,19 @@
+-- Creates a function SafeDiv that divides (and returns)
+-- the first by the second number or
+-- returns 0 if the second number is equal to 0.
+
+DELIMETER $$
+
+CREATE FUNCTION SafeDiv( a INT, b INT) RETURNS FLOAT DETERMINISTIC
+BEGIN
+    DECLARE result FLOAT;
+    if b != 0 THEN
+        SET result = a / b;
+    ELSE
+        SET result = 0;
+    ENDIF;
+
+    RETURN result;
+END;
+$$
+DELIMITER ;
