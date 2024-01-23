5deaeec005b56ecee899f462467885131214d2d6
millywanderi <wanderinyambura5@gmail.com>
2024-01-23 08:35:28 +0300

    inserts a new document in a collection based on kwargs

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+A module that inserts a new document in a collection based on kwargs
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    A function that inserts a new document in a collection based on kwargs
+    and returns the new _id
+    """
+    result = mongo_collection.insert_one(kwargs)
+    retun result.inserted_id
